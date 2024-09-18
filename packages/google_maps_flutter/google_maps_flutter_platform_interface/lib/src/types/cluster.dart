524ef67d935d3590e5857c0d1c032c14e19c8d45
NatanGameron <40638736+NatanGameron@users.noreply.github.com>
2024-09-18 19:14:35 -0300

    Support Specific Non Countable Markers Ids

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/cluster.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/cluster.dart
@@ -16,6 +16,7 @@ class Cluster {
     this.markerIds, {
     required this.position,
     required this.bounds,
+    this.nonCountableMarkerIds = const [],
   }) : assert(markerIds.length > 0);
 
   /// ID of the [ClusterManager] of the cluster.
@@ -27,15 +28,21 @@ class Cluster {
   /// The bounds containing all cluster markers.
   final LatLngBounds bounds;
 
-  /// List of [MarkerId]s in the cluster.
+  /// List of [MarkerId]s in the cluster that affect the count.
   final List<MarkerId> markerIds;
 
-  /// Returns the number of markers in the cluster.
+  /// List of [MarkerId]s in the cluster that do not affect the count.
+  final List<MarkerId> nonCountableMarkerIds;
+
+  /// Returns the number of markers in the cluster that affect the count.
   int get count => markerIds.length;
+
+  /// Returns the total number of markers (countable + non-countable).
+  int get totalMarkers => markerIds.length + nonCountableMarkerIds.length;
 
   @override
   String toString() =>
-      '${objectRuntimeType(this, 'Cluster')}($clusterManagerId, $position, $bounds, $markerIds)';
+      '${objectRuntimeType(this, 'Cluster')}($clusterManagerId, $position, $bounds, $markerIds, $nonCountableMarkerIds)';
 
   @override
   bool operator ==(Object other) {
@@ -43,10 +50,11 @@ class Cluster {
         other.clusterManagerId == clusterManagerId &&
         other.position == position &&
         other.bounds == bounds &&
-        listEquals(other.markerIds, markerIds);
+        listEquals(other.markerIds, markerIds) &&
+        listEquals(other.nonCountableMarkerIds, nonCountableMarkerIds);
   }
 
   @override
   int get hashCode =>
-      Object.hash(clusterManagerId, position, bounds, markerIds);
+      Object.hash(clusterManagerId, position, bounds, markerIds, nonCountableMarkerIds);
 }
